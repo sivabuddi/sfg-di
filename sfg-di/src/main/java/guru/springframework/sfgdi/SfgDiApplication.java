5a6671dce9c68031a0f9b71001acce4560af56bd
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-09-05 14:14:41 -0500

    Bean Life Cycle
     Constructor Instantiation
    setBeanName (BeanNameAware)
    setBeanFactoryName(BeanFactoryAware)
    setApplicationContext(ApplicationContextAware)
    postProcessBeforeIntializtion(BeanPostProcessor)
    PostConstruct Annotation
    afterPropertySet(InitialingBean)
    postProcessAfterInitialization(BeanPostProcessor)
    Bean Ready to use
    PreDestroy Annotation
    destroy(Disposable)

--- sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -14,8 +14,8 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext context =SpringApplication.run(SfgDiApplication.class, args);
 
-		PetController petController=(PetController) context.getBean("petController");
-		System.out.println(petController.whichPetIsTheBest());
+//		PetController petController=(PetController) context.getBean("petController");
+//		System.out.println(petController.whichPetIsTheBest());
 
 //		I18nController i18nController =(I18nController)context.getBean("i18nController");
 //		System.out.println(i18nController.sayHello());
