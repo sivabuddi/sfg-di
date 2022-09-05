d9e2fac3f89d277ef2633f759012de3c06b49713
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-09-05 12:21:06 -0500

    Spring profiles
    Use profile annotation along with changes in the application.property file in resource directory

--- sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectedController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,21 +13,25 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context =SpringApplication.run(SfgDiApplication.class, args);
-		MyController myController=(MyController)context.getBean("myController");
-		System.out.println("-----Primary Bean--------------");
-		System.out.println(myController.sayHello());
 
-		System.out.println("----Property Based Controller-----");
-		PropertyInjectedController propertyInjectedController=(PropertyInjectedController) context.getBean("propertyInjectedController");
-		System.out.println(propertyInjectedController.getGreeting());
+		I18nController i18nController =(I18nController)context.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
 
-		System.out.println("--Setter Based Controller----------");
-		SetterInjectedController setterInjectedController=(SetterInjectedController)context.getBean("setterInjectedController");
-		System.out.println(setterInjectedController.getGreeting());
-
-		System.out.println("----------Constructor Based Controller-----");
-		ConstructorInjectedController constructorInjectedController=(ConstructorInjectedController) context.getBean("constructorInjectedController");
-		System.out.println(constructorInjectedController.getGreeting());
+//		MyController myController=(MyController)context.getBean("myController");
+//		System.out.println("-----Primary Bean--------------");
+//		System.out.println(myController.sayHello());
+//
+//		System.out.println("----Property Based Controller-----");
+//		PropertyInjectedController propertyInjectedController=(PropertyInjectedController) context.getBean("propertyInjectedController");
+//		System.out.println(propertyInjectedController.getGreeting());
+//
+//		System.out.println("--Setter Based Controller----------");
+//		SetterInjectedController setterInjectedController=(SetterInjectedController)context.getBean("setterInjectedController");
+//		System.out.println(setterInjectedController.getGreeting());
+//
+//		System.out.println("----------Constructor Based Controller-----");
+//		ConstructorInjectedController constructorInjectedController=(ConstructorInjectedController) context.getBean("constructorInjectedController");
+//		System.out.println(constructorInjectedController.getGreeting());
 
 	}
 
